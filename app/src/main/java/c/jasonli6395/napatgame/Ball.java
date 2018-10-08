04e4d572cfdcbfe7dc5910064aade520d6544607
Jason Li <JasonLi6395@gmail.com>
2018-10-07 20:38:32 -0400

    Ball Movement Fixed

--- app/src/main/java/c/jasonli6395/napatgame/Ball.java
+++ app/src/main/java/c/jasonli6395/napatgame/Ball.java
@@ -1,4 +1,49 @@
 package c.jasonli6395.napatgame;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+
 public class Ball {
+
+    private int radius;
+    private Paint paint;
+    private int x, y;
+    private int xVel = 10, yVel = 5;
+    private int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+    private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+
+    public Ball() {
+        paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.RED);
+        radius = 25;
+        x = 100;
+        y = 100;
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawCircle(x, y, radius, paint);
+    }
+
+    public void update() {
+        if (x < 0 && y < 0) {
+            x = screenWidth / 2;
+            y = screenHeight / 2;
+        } else {
+            x += xVel;
+            y += yVel;
+            if (x > screenWidth - radius * 2 || x < 0) {
+                Log.d("X",Integer.toString(x));
+                xVel = xVel * -1;
+            }
+            if (y > screenHeight - radius * 2 || y < 0) {
+                Log.d("Y",Integer.toString(y));
+                yVel = yVel * -1;
+            }
+        }
+    }
 }
--- app/src/main/java/c/jasonli6395/napatgame/GameView.java
+++ app/src/main/java/c/jasonli6395/napatgame/GameView.java
@@ -1,7 +1,59 @@
 package c.jasonli6395.napatgame;
 
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.view.SurfaceView;
 import android.view.SurfaceHolder;
 
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+    private MainThread thread;
+    private Ball Ball;
+    public GameView(Context context) {
+        super(context);
+
+        getHolder().addCallback(this);
+
+        thread = new MainThread(getHolder(), this);
+        setFocusable(true);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        Ball = new Ball();
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        while (retry) {
+            try {
+                thread.setRunning(false);
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            retry = false;
+        }
+    }
+
+    public void update() {
+        Ball.update();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        if(canvas!=null){
+            Ball.draw(canvas);
+        }
+    }
 }
